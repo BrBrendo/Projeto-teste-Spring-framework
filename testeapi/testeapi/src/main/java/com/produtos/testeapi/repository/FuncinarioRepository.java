18625cea62f2547bb102ec466c83f6fdb5eb502a
Brendo Melo <88464238+BrBrendo@users.noreply.github.com>
2022-02-01 11:22:37 -0400

    Delete testeapi/testeapi/src/main/java/com/produtos/testeapi directory

--- testeapi/testeapi/src/main/java/com/produtos/testeapi/repository/FuncinarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.produtos.testeapi.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.produtos.testeapi.models.Funcionario;
-
-public interface FuncinarioRepository extends JpaRepository<Funcionario, long>{
-}
